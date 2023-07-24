95174c681940c58f9752dc70b41da9aac91a20e9
Shahd Abdalla <110117862+Shahdabdalla123@users.noreply.github.com>
2023-07-24 20:01:28 +0300

    Create form.php

---
+++ lab4/form.php
@@ -0,0 +1,96 @@
+<?php
+$errors=array();
+
+if(isset($_GET['errors']))
+{
+    $errors=json_decode($_GET['errors'],true);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" href="form.css">
+</head>
+<body>
+ <h2 > Registration Form</h2>
+ <form action="connect.php" method="POST" enctype="multipart/form-data">
+
+    <label for="name" >Name</label>
+      &nbsp; &nbsp;  &nbsp;    &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  
+    <input type="text" id="name" name="name" >
+    <div class="red">
+    <?php
+     if(isset($errors['name']))
+     {
+        echo $errors['name'];
+     }
+    ?>
+    </div>
+    <br> 
+    <label for="Email" >Email</label>
+     &nbsp; &nbsp;  &nbsp;    &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  
+    <input type="text" id="Email" name="Email" >
+    <div class="red">
+    <?php
+     if(isset($errors['Email']))
+     {
+        echo $errors['Email'];
+     }
+    ?>
+    </div>
+   
+    <br> 
+    <label for="password">Password:</label>
+     &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp;&nbsp;&nbsp; &nbsp;     
+  
+    <input type="password" id="password" name="password">
+    <div class="red">
+    <?php
+     if(isset($errors['password']))
+     {
+        echo $errors['password'];
+     }
+    ?>
+    </div>
+    <br>
+
+    <label for="cpassword">Confirm Password:</label>
+     &nbsp; &nbsp;
+    <input type="password" id="cpassword" name="cpassword"><br><br>
+    
+    <label for="Text">EXT</label>
+    &nbsp; &nbsp;  &nbsp;    &nbsp;    &nbsp;    &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;       
+    <input type="Text" id="Text" name="Text"><br><br>
+
+    
+    <label for="Rm">Room Num:</label>
+    &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp;  &nbsp;  &nbsp;    
+    <select id="Rm" name="Rm">
+      <option value="R1">Room 1</option>
+      <option value="R2">Room 2</option>
+      <option value="R3">Room 3</option>
+      <option value="R4">Room 4</option>
+    </select><br><br>
+ 
+    <label for="img">Image:</label>
+    &nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;  &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;  &nbsp; &nbsp;&nbsp;  
+    <input type="file" id="img" class="img" name="image">
+    <div class="red">
+    <?php
+     if(isset($errors['image']))
+     {
+        echo $errors['image'];
+     }
+    ?>
+    </div>
+    <br><br>
+    
+    <button class="b"type="submit">Register</button>
+  </form>
+  <button class="b"><a style="text-decoration:none; color:black;" href="Table.php">Table</a></button>
+  
+</body>
+</html>
